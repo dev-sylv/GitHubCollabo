73bd0f2aabe61ed452c30295afd2f00943eeafb2
ebenezer aladesuyi <ebenezeraladesuyi@gmail.com>
2023-03-08 17:33:28 +0100

    break component

---
+++ src/Components/Break/Break.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Break.css'
+
+const Break = () => {
+    return (
+        <div className='break'>
+            <div className='break-hold'>
+            <div className='brk'>
+                <h1>Break into Tech like a pro</h1>
+                <button>Enrol Now</button>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Break;
--- src/Components/HomeScreen/HomeScreen.jsx
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from "../Hero/Hero"
+import Hero from "../Break/Break"
 
 const HomeScreen = () => {
   return (
     <div>
 
         <Hero />
+        <Break />
 
 
     </div>
